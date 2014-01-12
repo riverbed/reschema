2d51384bb5757038bb3c7b92348292913cabc54f
Henry Andrews <handrews@riverbed.com>
2014-01-11 17:16:57 -0800

    Rework packaging for internal pypibox.
    This moves reschema over to the system used by the PQ infrastructure.
    This is mostly for internal packaging, and some further work needs to be
    done to sort out open-source packaging.

--- setup.py
+++ setup.py
@@ -1,24 +1,51 @@
+import os
+import pip
+import sys
+
+from setuptools.command.test import test as TestCommand
+from pip.req import parse_requirements
+
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-from contrib.version import get_git_version
+def requirements():
+    return [str(ir.req) for ir in parse_requirements('requirements.txt')]
+    
+def get_version():
+    try:
+        # pip-installed packages can get the version from the 'version.txt'
+        # file, since it is included in the package MANIFEST.
+        with open('version.txt', 'r') as f:
+            return f.read().strip()
+    except IOError:
+        # since 'version.txt' is .gitignored, running setup.py (install|develop)
+        # from a git repo requires a bit of bootstrapping. in this case, we use
+        # the raw .git tag as the version.
+        pip.main(['install', '-U', '-i http://pypi/pq/development/', 'pq-ci'])
+        from pq_ci import git
+        tag = git.parse_tag()
+        return '-'.join([tag['version'], tag['commits'], tag['sha']])
 
-setup(name="reschema",
-      version=get_git_version(),
-      description="reschema - Parse REST API schema into Python objects, support documentation generation",
-      author="Riverbed Technology",
-      author_email="cwhite@riverbed.com",
+readme = open('README.rst').read()
 
-      packages=find_packages(),
-      scripts=[
-          'bin/reschema-doc'
-      ],
-      install_requires=[
-          "uritemplate>=0.6",
-          "PyYAML==3.10",
-          "Markdown>=2.2.1",
-          "jsonpointer"
-      ],
-      include_package_data=True)
+setup(
+    name='reschema',
+    version=get_version(),
+    description="reschema - Parse REST API schema into Python objects, support documentation generation",
+    long_description=readme,
+    author="Riverbed Technology",
+    author_email="cwhite@riverbed.com",
+    packages=[
+        'reschema',
+    ],
+    package_dir={'reschema': 'reschema'},
+    scripts=[
+        'bin/reschema-doc'
+    ],
+    include_package_data=True,
+    install_requires=requirements(),
+    keywords='reschema',
+    tests_require=['pytest', 'mock'],
+)
