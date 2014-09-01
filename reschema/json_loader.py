f1bdc6b8ba2b1937dfed1811811d909cd5dbb21d
Thom Gerdes <tgerdes@riverbed.com>
2014-08-31 20:53:09 -0700

    Relint json parser overhaul
    Improve the marks added by the json_loader.  The Yaml Loader marks
    strings, objects, and arrays, so we should do the same with json to give
    it just as much love.
    
    This makes the output of relint <schema>.json  much more consumable by
    external tools.

--- reschema/json_loader.py
+++ reschema/json_loader.py
@@ -1,10 +1,11 @@
 import json
-import inspect
 from collections import OrderedDict
 
 from yaml.error import Mark
 from json.scanner import py_make_scanner
-from json.decoder import JSONDecoder
+import json.decoder
+
+from reschema.loader_nodes import ordered_dict_node, list_node, unicode_node
 
 
 def linecol(doc, pos):
@@ -17,36 +18,51 @@ def linecol(doc, pos):
     return lineno, colno
 
 
-class Decoder(JSONDecoder):
-    def __init__(self, *args, **kwargs):
+class Decoder(json.decoder.JSONDecoder):
+    def __init__(self, name="", *args, **kwargs):
         super(Decoder, self).__init__(*args, **kwargs)
+
+        def wrap_obj_parser(parser, node_type):
+            def internal(o_and_start, *args, **kwargs):
+                o, start = o_and_start
+                r, end = parser(o_and_start, *args, **kwargs)
+
+                start_line, start_col = linecol(o, start)
+                end_line, end_col = linecol(o, end)
+                start_mark = Mark(name, start, start_line, start_col, o, start)
+                end_mark = Mark(name, end, end_line, end_col, o, start)
+
+                return node_type(r, start_mark, end_mark), end
+            return internal
+
+        def wrap_parser(parser, node_type):
+            def internal(o, start, *args, **kwargs):
+                r, end = parser(o, start, *args, **kwargs)
+
+                start_line, start_col = linecol(o, start)
+                end_line, end_col = linecol(o, end)
+                start_mark = Mark(name, start, start_line, start_col, o, start)
+                end_mark = Mark(name, end, end_line, end_col, o, start)
+
+                return node_type(r, start_mark, end_mark), end
+            return internal
+
+        self.parse_string = wrap_parser(self.parse_string, unicode_node)
+        self.parse_array = wrap_obj_parser(self.parse_array, list_node)
+        self.parse_object = wrap_obj_parser(self.parse_object,
+                                            ordered_dict_node)
+
+        # Not thread safe, but need to patch this for loading marks onto object
+        # keys.
+        json.decoder.scanstring = self.parse_string
+
+        # Need to hook the python scanner because the C scanner doesn't have
+        # a hookable method to parse_string.
         self.scan_once = py_make_scanner(self)
 
 
-def obj_hook(obj):
-    """Hook to create OrderedDicts with start/end marks."""
-    # this approach requires the json python scanner
-    # the c-module doesn't lend itself to frame inspection
-    # which means we don't get the line-by-line info needed
-    # to create good marks
-
-    frame = inspect.currentframe().f_back
-    s = frame.f_locals['s']
-    start = frame.f_locals['s_and_end'][1] - 1
-    end = frame.f_locals['end']
-
-    o = OrderedDict(obj)
-    line, col = linecol(s, start)
-    o.start_mark = Mark('JSONObject', start, line, col, s, start)
-    line, col = linecol(s, end)
-    o.end_mark = Mark('JSONObject', end, line, col, s, end)
-
-    return o
-
-
 def marked_load(stream):
-    return json.load(stream, cls=Decoder,
-                     object_hook=obj_hook, object_pairs_hook=obj_hook)
+    return json.load(stream, cls=Decoder, name=stream.name)
 
 
 def clean_load(stream):
