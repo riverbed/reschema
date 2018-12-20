bfbb65c7b69d42be1d1d1d086edb703cad30618b
Mike Garabedian <mike.garabedian@riverbed.com>
2018-12-20 14:21:10 -0500

    Update python version

--- setup.py
+++ setup.py
@@ -61,8 +61,8 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Documentation',
     ],
+    python_requires='>3.5.0',
 )
